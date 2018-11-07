63c95b728565dd9661ffac0560bbebf0455503f2
LiHao <1609216660@qq.com>
2018-11-07 16:38:25 +0800

    update

--- src/components/content/socket/Socket.vue
+++ src/components/content/socket/Socket.vue
@@ -1,14 +1,54 @@
 <template>
-    <div>
+    <div class="container">
         <div>这是socket</div>
-        
-        <el-button @click="fetch('list')">拉数据</el-button>
-        <el-button @click="edit">修改</el-button>
+        <div class="single" :model="task" v-for="(item,index) in task" :key="index" v-if="item">
+            <el-row>
+                <el-col :span="16">
+                    <el-card shadow="never" style="text-align:center;border:0">
+                    姓名：<el-button @click="fetch('text','name',index)"><span v-html="item.name?item.name:''"></span></el-button>
+                    </el-card>
+                </el-col>
+            </el-row>
+                <el-row>
+                <el-col :span='4'>
+                    <el-card shadow="never" style="border:0">
+                    <el-button @click="fetch('text','today',index)">今天要做的事：</el-button>
+                    </el-card>
+                    </el-col>
+                <el-col :span="12">                    
+                    <span v-html="item.today?item.today:''"></span>                    
+                </el-col>
+                </el-row>
+                <br>
+                <el-row>
+                <el-col :span='4'>
+                    <el-card shadow="never" style="border:0">
+                    <el-button @click="fetch('text','tomorrow',index)">明天要做的事：</el-button>
+                    </el-card>
+                    </el-col>
+                <el-col :span="12">
+                   
+                    <span v-html="item.tomorrow?item.tomorrow:''"></span>
+                    
+                </el-col>
+                </el-row>
+                <br>
+                <el-row>
+                <el-col :span='4'>
+                    <el-card shadow="never" style="border:0">
+                    <el-button @click="fetch('text','question',index)">遇到的问题：</el-button>
+                    </el-card>
+                    </el-col>
+                <el-col :span="12">                    
+                    <span v-html="item.question?item.question:''"></span>                    
+                </el-col>
+            </el-row>
+            <hr>
+        </div>
+        <el-button @click="remove">删除</el-button>
         <el-button @click="add">增加</el-button>
-        <UEditor :config="config" ref="ueditor" class="ueditor" @change="contentChange"></UEditor>
-        
-        <p v-for="item in msgs" :key="item.id" v-html="item"></p>
-        <el-button type="primary" class="send" style="" @click="sendMsg">发<br/><br/>送</el-button>
+        <UEditor :config="config" ref="ueditor" class="ueditor" @change="contentChange"></UEditor>        
+        <el-button type="primary" class="send" style="" @click="sendMsg">发送</el-button>
     </div>
 </template>
 
@@ -17,7 +57,7 @@ import io from 'socket.io-client';
 import UEditor from "@/components/common-components/ueditor.vue";
 import Interface from '@/util/interface.js';
 var status = 0;
-var socket = io('39.107.233.105:3002',{
+var socket = io('http://localhost:3002',{
     path:'/socket',
     autoConnect:false,
 });
@@ -50,6 +90,9 @@ export default {
                 UEDITOR_HOME_URL: "static/ueditor/" //静态资源相对路径
             },
             msgs:[],
+            task:[],
+            type:'',
+            index:0,
         }
     },
     created(){
@@ -59,34 +102,40 @@ export default {
         socket.open();
         var _this =this   
         //监听广播消息
-        socket.on('update',function(data){
-            if(data.content){
-                setTimeout(()=>{
-                    console.log(data.content,'接受的信息')                
-                    _this.msgs.push(data.content)                    
-                },150)
-            }
+        socket.on('update',function(data){            
+            _this.fetch('list')
         })
     },
     methods:{
-        fetch(scence){
+        fetch(scence,type,index){
             if(scence == 'list'){
                 let params = {
-                    id: 5
+                    id: sessionStorage.getItem('id')
                 }
                 Interface.getData('get',Interface.task.list,params).then(res=>{
                     if(res.data.code == 200){
-
+                        this.task = JSON.parse(res.data.data[0].task)
+                        console.log(this.task)
                     }
                 })
+            }else if(scence == 'text'){
+                this.index = index;
+                this.type= type;
+                this.$refs.ueditor.setContent(this.task[index][type])
+                console.log(this.task[index][type])
             }
         },
+        reset(){
+            this.type = '';
+            this.index = 0;
+            this.$refs.ueditor.setContent('')
+        },
         sendMsg(){            
-            //给connection连接发送一条消息
-            socket.emit('mes',{editor:this.$refs.ueditor.getContent()})
-            setTimeout(()=>{
-                this.$refs.ueditor.setContent(``)
-            },100)
+            // 给connection连接发送一条消息
+            
+            this.task[this.index][this.type] = this.$refs.ueditor.getContent()
+            this.edit();
+
         },
         getContent() {
             let content = this.$refs.ueditor.getContent();
@@ -96,32 +145,33 @@ export default {
             // console.log(data)
             // socket.emit('mes',{editor:data,msg:'这是编辑器的最新内容'})
         },
-        remove(){
-            let params = {
-                _methods:'delete',
-                id:5
-            }
-            Interface.getData('post',Interface.task.list,params).then(res=>{
-
-            })
+        remove(index){
+            this.task.splice(index,1);
+            this.edit();
         },
         add(){
-            let params = {
-                task:'这是一个测试信息，看能不能加进去'
-            }
-            Interface.getData('post',Interface.task.list,params).then(res=>{
-
+            this.task.push({
+                name:"",
+                today:'',
+                tomorrow:'',
+                question:''
             })
+            this.edit()
         },
-        edit(id){
-            id=1
+        edit(){
+            let c = JSON.stringify(this.task)
+            console.log(c,typeof c)
             let params = {
-                task:'这是一个修改信息，看能不能加进去',
-                id: 5,
+                task:JSON.stringify(this.task),
+                id: sessionStorage.getItem('id'),
                 _methods:"put"
             }
+            var that = this
             Interface.getData('post',Interface.task.list,params).then(res=>{
-
+                if(res.data.code==200){                    
+                    socket.emit('mes',{editor:that.$refs.ueditor.getContent()})
+                    that.reset();
+                }
             })
         },
     }
@@ -129,16 +179,27 @@ export default {
 </script>
 
 <style scoped>
+.container{
+    position: relative;
+    min-height: 100vh;
+}
 .ueditor{
-    margin-left: 120px;
-    position: absolute;
-    bottom: 10px;
+    width: 100%;
+    position: sticky;
+    bottom: 50px;
+    left: 120px;
 }
 .send{
-    width:50px;
-    height:250px;
-    position:absolute;
-    bottom: 10px;
-    left: 70px;
+    width:100%;
+    height:50px;
+    position:sticky;
+    bottom: 0px;
+    left: 120px;
+}
+.el-row{
+    margin-bottom: 10px;
+}
+.focus{
+    outline:-webkit-focus-ring-color auto 5px;
 }
 </style>
--- src/components/content/socket/SocketList.vue
+++ src/components/content/socket/SocketList.vue
@@ -1,18 +1,31 @@
 <template>
-     <el-table
+<div class="container">
+    <el-button @click="edit('add')">新增文档</el-button>
+    <el-table
       :data="tableData"
       style="width: 100%">
       <el-table-column
         prop="id"
         label="id"
-        width="180">
+        width="">
       </el-table-column>
       <el-table-column
         prop="created_at"
         label="创建时间"
-        width="180">
+        width="">
       </el-table-column>
+       <el-table-column
+      fixed="right"
+      label="操作"
+      width="100">
+      <template slot-scope="scope">
+        <el-button @click="remove(scope.row)" type="text" size="small">删除</el-button>
+        <el-button type="text" @click="edit('edit',scope.row)" size="small">编辑</el-button>
+      </template>
+    </el-table-column>
     </el-table>
+</div>
+     
 </template>
 <script>
 import Interface from '@/util/interface.js';
@@ -32,13 +45,38 @@ export default {
 
                 }
                 Interface.getData('get',Interface.task.list,params).then(res=>{
-                    console.log(res)
-                    if(res.code == 200){
-                        this.tableData = res.data
-                        console.log(res,123)
+                    if(res.data.code == 200){
+                        this.tableData = res.data.data
                     }
                 })
             }
+        },
+        remove(row){
+            let params= {
+                id: row.id,
+                _methods: 'delete'
+            };
+            Interface.getData('post',Interface.task.list,params).then(res=>{
+                if(res.data.code==200){
+                    this.fetch('list')
+                }
+            })
+        },
+        edit(type,row){
+            sessionStorage.setItem('type',type)
+            if(type == 'edit'){
+                sessionStorage.setItem('id',row.id)
+            }else{
+                let arr = []
+                let params = {
+                    task:JSON.stringify(arr)
+                }
+                Interface.getData('post',Interface.task.list,params).then(res=>{
+                    console.log(res)
+                    sessionStorage.setItem('id',res.data.data.insertId)
+                })
+            }
+            this.$router.push('/socket')
         }
     }
 }
